3c5116986177084671af6b961b9e5526882e0cdf
Muhammad Saad Ahmad <muhammadsaadahmad1@example.com>
2026-06-14 19:48:31 +0500

    recccntttt

--- start-localhost.bat
+++ start-localhost.bat
@@ -17,6 +17,6 @@ echo Press Ctrl+C to stop the server
 echo.
 
 cd /d "%~dp0"
-npx live-server --port=5500 --host=localhost --open=/index.html --quiet
+npx live-server --port=5500 --host=0.0.0.0 --open=/index.html --quiet
 
 pause
